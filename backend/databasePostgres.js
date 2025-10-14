e8df85340b56100e7473428c4d2ce1c35abb35ee
jongaskk <joaogustavogetao@gmail.com>
2025-10-14 08:53:00 -0300

    Add files via upload

---
+++ backend/databasePostgres.js
@@ -0,0 +1,30 @@
+import { randomUUID } from "node:crypto";
+import { sql } from "./sql.js";
+import bcrypt from "bcrypt";
+
+export class DatabasePostgres{
+    async list(){
+        try{
+            const result = await sql `SELECT * FROM users`;
+            return result;
+        }catch (err){
+            console.log("Erro ao listar usuários: ", err);
+            return[];
+        }
+    }
+
+    async create(user){
+        const userId = randomUUId();
+        const { name, email, password } = user;
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        await sql `
+            INSERT INTO users (id, name, email,password) VALUES (${userId}, ${name}, ${email}, ${hashedPassword})`;
+    }
+
+    async findByEmail(email){
+        const result = await
+    }
+
+    
+}
